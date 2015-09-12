30efe8de6b5e659698072235cae625ef0df880f1
Samuel Overington (neon) <so@madeso.uk>
2015-09-12 22:01:27 +0100

    sass working properly. to compile - the gulp file doesn't requires sass rather than compass

--- functions.php
+++ functions.php
@@ -121,6 +121,15 @@ function _s_widgets_init() {
 		'before_title'  => '<h1 class="widget-title">',
 		'after_title'   => '</h1>',
 	) );
+	register_sidebar( array(
+		'name'          => __( 'Partner Directory', '_s' ),
+		'id'            => 'vcsassist_partner_directory',
+		'description'   => 'VCS Assist Partner Directory sidebar',
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
 }
 add_action( 'widgets_init', '_s_widgets_init' );
 

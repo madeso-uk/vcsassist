3d353106af629d9aa930722cf53d25093dba612d
Samuel Overington (neon) <so@madeso.uk>
2015-09-14 08:28:05 +0100

    added partner directory pge and side-bar

---
+++ partner_directory_page.php
@@ -0,0 +1,37 @@
+<?php
+/*
+* Template Name: Partner Directory Full page template
+* */
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package _s
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="full-width content-area">
+		<main id="main" class="site-main" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php /* get_template_part( 'content', 'page' ); */ ?>
+            <?php wpbdp_render_listing(null, 'single', false) ?>
+
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+<?php get_sidebar('directory'); ?>
+<?php get_footer(); ?>
---
+++ sidebar-directory.php
@@ -0,0 +1,5 @@
+<?php if ( is_active_sidebar( 'vcsassist_partner_directory' ) ) : ?>
+	<div id="vcs-pd" class="partner-directory widget-area" role="complementary">
+		<?php dynamic_sidebar( 'vcsassist_partner_directory' ); ?>
+	</div><!-- #vcs-pd -->
+<?php endif; ?>

3615d415d66eee2e3a2dacd607a3c21e75e05c6f
Samuel Overington (neon) <so@madeso.uk>
2015-09-23 23:33:52 +0100

    added business directory template files, as well as page php files that call them. add list-category template files edited layout of partner directory pages

--- partner_directory_page.php
+++ partner_directory_page.php
@@ -19,7 +19,7 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
+				<?php get_template_part( 'content', 'directorypage' ); ?>
 
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template
--- sidebar-directory.php
+++ sidebar-directory.php
@@ -1,6 +1,8 @@
 	<div id="sidebar-vcspd" class="sidebar partner-directory widget-area" role="complementary">
-  <?php wpbdp_the_directory_categories(); ?>
-<?php if ( is_active_sidebar( 'vcsassist_partner_directory' ) ) : ?>
-		<?php dynamic_sidebar( 'vcsassist_partner_directory' ); ?>
-<?php endif; ?>
+    <?php wpbdp_the_main_links(); ?>
+    <?php wpbdp_the_search_form(); ?>
+    <?php wpbdp_the_directory_categories(); ?>
+    <?php if ( is_active_sidebar( 'vcsassist_partner_directory' ) ) : ?>
+        <?php dynamic_sidebar( 'vcsassist_partner_directory' ); ?>
+    <?php endif; ?>
 	</div><!-- #sidebar-vcspd -->

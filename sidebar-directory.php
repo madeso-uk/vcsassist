3fe60b068ebe82a1cef4eacc5b17a60884a05d30
Samuel Overington (neon) <so@madeso.uk>
2015-09-27 17:00:14 +0100

    added sidebar-directory - functions rely on vcsplugin

--- sidebar-directory.php
+++ sidebar-directory.php
@@ -1,7 +1,8 @@
 	<div id="sidebar-vcspd" class="sidebar partner-directory widget-area" role="complementary">
-    <?php wpbdp_the_main_links(); ?>
+    <?php vcs_submitbutton(); ?>
     <?php wpbdp_the_search_form(); ?>
-    <?php wpbdp_the_directory_categories(); ?>
+    <?php // wpbdp_the_directory_categories(); ?>
+    <?php vcs_categories_button(); ?>
     <?php if ( is_active_sidebar( 'vcsassist_partner_directory' ) ) : ?>
         <?php dynamic_sidebar( 'vcsassist_partner_directory' ); ?>
     <?php endif; ?>

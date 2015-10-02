c8638ebb62bf708132d47b35791ee23a14d55e26
Samuel Overington (neon) <so@madeso.uk>
2015-10-02 13:34:49 +0100

    added the main page of partner-directory

---
+++ businessdirectory-main-page.tpl.php
@@ -0,0 +1,21 @@
+<div id="wpbdp-main-page" class="wpbdp-main-page wpbdp-main businessdirectory wpbdp-page <?php echo join(' ', $__page__['class']); ?>">
+
+          <?php // wpbdp_the_main_links(); ?>
+          <?php // wpbdp_the_search_form(); ?>
+
+    <?php echo $__page__['before_content']; ?>
+
+    <div class="wpbdp-page-content <?php echo join(' ', $__page__['content_class']); ?>">
+    		<?php // wpbdp_the_directory_categories(); ?>
+
+        <?php if ($listings) { ?>
+  <header class='listing-excerpt'>
+    <span>Title</span>
+    <span>description</span>
+  </header>
+<?php
+        echo $listings;
+        } ?>
+    </div>
+
+</div>

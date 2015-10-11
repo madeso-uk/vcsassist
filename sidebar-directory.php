37fbf6205fd6916b185f68b9405a4c448205a047
Samuel Overington (neon) <so@madeso.uk>
2015-10-11 17:38:33 +0100

    directory single page styling sidebar direcotry link styling added mixin for button-style

--- sidebar-directory.php
+++ sidebar-directory.php
@@ -1,7 +1,8 @@
 	<div id="sidebar-vcspd" class="sidebar partner-directory widget-area" role="complementary">
+    <h3>Browse Partners</h3>
+    <?php vcspd_the_search_form(); ?> 
     <?php vcs_categories_button(); ?>
-<h3>Search Partners</h3>
-    <?php wpbdp_the_search_form(); ?>
+    <?php vcs_tag_button(); ?>
     <?php vcs_submitbutton(); ?>
     <?php if ( is_active_sidebar( 'vcsassist_partner_directory' ) ) : ?>
         <?php dynamic_sidebar( 'vcsassist_partner_directory' ); ?>

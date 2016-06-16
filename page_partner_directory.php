a06bcbd7c0fc3f04c5df5f7867baf4b7c10fe74a
Samuel Overington (neon) <so@madeso.uk>
2016-06-16 20:47:23 +0100

    Changed name of partner diretory page template Removed un-needed text in sidebar,

--- page_partner_directory.php
+++ page_partner_directory.php
@@ -1,16 +1,14 @@
 <?php
 /*
-* Template Name: Partner Directory Full page template
+* Template Name: Partner Directory page with sidebar template
 * */
+
 /**
- * The template for displaying all pages.
+ * The Template for the main partner directory page.
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+ * This is the main partner directory template - which
+ * includes the sidebar
  *
- * @package _s
  */
 
 get_header(); ?>
--- sidebar-directory.php
+++ sidebar-directory.php
@@ -1,9 +1,6 @@
 	<div id="sidebar-vcspd" class="sidebar partner-directory widget-area" role="complementary">
     <h3>Browse Partners</h3>
-    <?php vcspd_the_search_form(); ?> 
-    <?php // vcs_categories_button(); ?>
-    <?php // vcs_tag_button(); ?>
-    <?php vcs_submitbutton(); ?>
+    <?php echo vcspd_search_form(); ?> 
     <?php if ( is_active_sidebar( 'vcsassist_partner_directory' ) ) : ?>
         <?php dynamic_sidebar( 'vcsassist_partner_directory' ); ?>
     <?php endif; ?>
